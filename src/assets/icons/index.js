84f03496e7cd00d1ddfe4eabc82109e0a8996ff8
Dafa Zakhulhaq <dafa27890@gmail.com>
2022-08-31 11:07:22 +0700

    import assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import IconCheck from './icon_check.svg';
+import IconMore from './icon_more.svg';
+import IconPlusRounded from './icon_plus_rounded.svg';
+import IconPlus from './icon_plus.svg';
+
+export {
+    IconCheck,
+    IconMore,
+    IconPlusRounded,
+    IconPlus
+}
