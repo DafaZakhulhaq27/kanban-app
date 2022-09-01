ec72c019e45ab9ac5f3be4da90cac2c4e993b020
Dafa Zakhulhaq <dafa27890@gmail.com>
2022-09-01 09:18:13 +0700

    update assets

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,11 +3,19 @@ import IconMore from './icon_more.svg';
 import IconPlusRounded from './icon_plus_rounded.svg';
 import IconPlus from './icon_plus.svg';
 import IconDanger from './icon_danger.svg';
+import IconArrowLeft from './icon_arrow_left.svg';
+import IconArrowRight from './icon_arrow_right.svg';
+import IconEdit from './icon_edit.svg';
+import IconTrash from './icon_trash.svg';
 
 export {
     IconCheck,
     IconMore,
     IconPlusRounded,
     IconPlus,
-    IconDanger
+    IconDanger,
+    IconArrowLeft,
+    IconArrowRight,
+    IconEdit,
+    IconTrash
 }
